6d4ea199bf2db1370a599d5d357d736dd0120b00
joshschcom <84538564+joshschcom@users.noreply.github.com>
2022-07-08 12:12:24 +0200

    Create FundMe.sol

---
+++ FundMe.sol
@@ -0,0 +1,59 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+import "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+   
+    uint256 public minimumUsd = 50 * 1e18; // 1 * 10 ** 18
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        // Want to be able to set a minimum fund amount in Usd
+        // How do we send ETH to this contract?
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn`t send enough!"); //1e18 == 1 * 10 **18
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;   
+    }
+
+    function withdraw() public onlyOwner {
+        
+        /* starting Index, ending index, step amount */
+        //funderindex++ is the same as funderIndex = funderIndex + 1
+        for(uint256 funderIndex =0; funderIndex < funders.length; funderIndex++){
+            //code
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        //reset array
+        funders = new address[](0);
+
+        //actually withdraw funds
+        //transfer
+        //payable(msg.sender).transfer(address(this).balance);
+        //send
+        //bool sendSuccess = payable(msg.sender).transfer(address(this).balance);
+        //require(sendSuccess, "Send failed");
+        //call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
+    }
+
+
+    //function withdrawal
+}
