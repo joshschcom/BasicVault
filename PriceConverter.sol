06150bc92cf50a6e48f416c5b15eeed4abf17643
joshschcom <84538564+joshschcom@users.noreply.github.com>
2022-07-08 12:13:54 +0200

    Update PriceConverter.sol

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+//https://docs.chain.link/docs/get-the-latest-price/
+
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
